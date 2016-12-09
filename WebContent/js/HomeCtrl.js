8f22ec7cdef759501a9df02d176cbc40c84b0885
RAJESH <rajesh.rapelly446@gmail.com>
2016-12-09 18:18:35 +0530

    Creation of the Chat from the front end

--- WebContent/js/HomeCtrl.js
+++ WebContent/js/HomeCtrl.js
@@ -43,8 +43,35 @@ app.config(function($routeProvider) {
     	templateUrl: "html/request.html",
     	controller:'myfriendctrl'
     })
+    
+    .when('/chat', {
+    templateUrl : 'html/Chat.html',
+    controller  : 'chatController'
+ });
 
 
 
-  .otherwise({redirectTo: '/'});
+  /*.otherwise({redirectTo: '/'});*/
+  
+  console.log("route");    });
+run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
+function run($rootScope, $location, $cookieStore, $http) {
+    // keep user logged in after page refresh
+    $rootScope.globals = $cookieStore.get('globals') || {};
+    $rootScope.currentuser = $cookieStore.get('currentuser') || {};
+    if ($rootScope.globals.currentUser) {
+        $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+    }
+
+  
+  
+  $rootScope.$on('$locationChangeStart', function (event, next, current) {
+      // redirect to login page if not logged in and trying to access a restricted page
+      var restrictedPage = $.inArray($location.path(), ['/login', '/register','/Home','/jobs']) === -1;
+      var loggedIn = $rootScope.globals.currentUser;
+      if (restrictedPage && !loggedIn) {
+          $location.path('/login');
+      }
 });
+  
+}
